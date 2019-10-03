a237c18e9b39612267c1ff9085b9b9b14ceb1d63
lan0017 <1909237392@qq.com>
2019-10-03 10:28:58 +0800

    first commit

--- cmake/app/boilerplate.cmake
+++ cmake/app/boilerplate.cmake
@@ -99,7 +99,7 @@ include(${ZEPHYR_BASE}/cmake/version.cmake)  # depends on hex.cmake
 #
 
 include(${ZEPHYR_BASE}/cmake/python.cmake)
-include(${ZEPHYR_BASE}/cmake/git.cmake)  # depends on version.cmake
+#include(${ZEPHYR_BASE}/cmake/git.cmake)  # depends on version.cmake
 include(${ZEPHYR_BASE}/cmake/ccache.cmake)
 
 if(${CMAKE_CURRENT_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_BINARY_DIR})
